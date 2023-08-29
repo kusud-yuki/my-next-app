16e878c4f257caff1a4722df4e1e29cddabe713a
kusud-yuki <u.b_bfg@icloud.com>
2023-08-29 14:28:41 +0900

    リストの優先度設定とフィルタリング機能を追加

--- react-app-example/src/ListApp.js
+++ react-app-example/src/ListApp.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 function ListApp() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [priority, setPriority] = useState('low');
+  const [showCompleted, setShowCompleted] = useState(true);
 
   const addTask = () => {
     if (newTask.trim() !== '') {
-      setTasks([...tasks, { text: newTask, completed: false }]);
+      setTasks([...tasks, { text: newTask, completed: false, priority }]);
       setNewTask('');
     }
   };
@@ -17,9 +19,24 @@ function ListApp() {
     setTasks(updatedTasks);
   };
 
+  const changePriority = (index, newPriority) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].priority = newPriority;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const filteredTasks = tasks.filter(
+    (task) => showCompleted || !task.completed
+  );
+
   return (
     <div>
-      <h2>リスト表示アプリ</h2>
+      <h2>ToDoリストアプリ</h2>
       <div>
         <input
           type="text"
@@ -27,10 +44,25 @@ function ListApp() {
           onChange={(e) => setNewTask(e.target.value)}
           placeholder="新しい項目を入力してください"
         />
+        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+          <option value="low">低</option>
+          <option value="medium">中</option>
+          <option value="high">高</option>
+        </select>
         <button onClick={addTask}>追加</button>
       </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={showCompleted}
+            onChange={() => setShowCompleted(!showCompleted)}
+          />
+          完了済みタスクを表示しない
+        </label>
+      </div>
       <ul>
-        {tasks.map((task, index) => (
+        {filteredTasks.map((task, index) => (
           <li
             key={index}
             style={{
@@ -39,7 +71,16 @@ function ListApp() {
             }}
             onClick={() => toggleTask(index)}
           >
-            {task.text}
+            {task.text} (優先度: {task.priority})
+            <button onClick={() => deleteTask(index)}>削除</button>
+            <select
+              value={task.priority}
+              onChange={(e) => changePriority(index, e.target.value)}
+            >
+              <option value="low">低</option>
+              <option value="medium">中</option>
+              <option value="high">高</option>
+            </select>
           </li>
         ))}
       </ul>
